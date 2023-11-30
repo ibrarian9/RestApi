37bf8b83ccae9a217f053222df465ff620bb4d02
ibrarian9 <ibrarian9@gmail.com>
2023-11-30 08:59:04 +0700

    FIX BEBERAPA BUGS

---
+++ src/main/java/com/example/restapi/Service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.restapi.Service;
+
+import com.example.restapi.Models.User;
+import com.example.restapi.Repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepo;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User Not Found : " + username));
+        return UserDetailsImpl.build(user);
+    }
+}
